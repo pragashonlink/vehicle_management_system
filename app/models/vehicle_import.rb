aedf6135e2d907e334417ba2385339617c9c4dc2
Pragash Rajarathnam <pragashonlink@gmail.com>
2021-10-24 14:05:43 +0530

    Integrate file upload and import data to vehicles table

--- app/models/vehicle_import.rb
+++ app/models/vehicle_import.rb
@@ -1,6 +1,32 @@
+require 'csv'
+
 class VehicleImport
   include ActiveModel::Model
+  include ActiveModel::Validations
 
   attr_accessor :import_file
   validates :import_file, presence: true
+
+  def save
+    vehicles = []
+
+    converter = lambda { |header| header.underscore.to_sym }
+    CSV.foreach(import_file.path, headers: true, header_converters: converter) do |csv_row|
+      row = csv_row.to_hash
+
+      row.merge!({
+        created_at: Time.now,
+        updated_at: Time.now
+      })
+
+      vehicles << row
+    end
+
+    Vehicle.upsert_all(
+      vehicles
+    )
+
+    true
+  end
+
 end
--- spec/models/vehicle_import_spec.rb
+++ spec/models/vehicle_import_spec.rb
@@ -4,10 +4,23 @@ RSpec.describe VehicleImport, type: :model do
   describe '#upload' do
     let(:upload_file) { Rack::Test::UploadedFile.new('spec/fixtures/vehicles_test_file.csv', 'application/csv') }
     subject { VehicleImport.new }
+    let(:vehicle_count) { Vehicle.count }
 
     it 'has an attachment' do
       subject.import_file = upload_file
       expect(subject).to be_valid
+    end
+
+    it 'does not have an attachment' do
+      expect(subject).to_not be_valid
+    end
+
+    it 'imports data' do
+      expect(subject).to_not be_valid
+
+      subject.import_file = upload_file
+      expect(subject.save).to eq(true)
+      expect(vehicle_count).to be > 0
     end
   end
 end
