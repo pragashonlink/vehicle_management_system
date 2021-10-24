f28ee862acb7a6c0fd306c42c1c98677cfa0972f
Pragash Rajarathnam <pragashonlink@gmail.com>
2021-10-24 15:08:59 +0530

    import vehicle data successfully

--- app/controllers/vehicle_imports_controller.rb
+++ app/controllers/vehicle_imports_controller.rb
@@ -1,6 +1,22 @@
 class VehicleImportsController < ApplicationController
-
   def new
+    @vehicle_import = VehicleImport.new
+  end
+
+  def create
+    @vehicle_import = VehicleImport.new(permitted_params)
+    
+    if !@vehicle_import.save
+      render :new
+    else
+      puts "done"
+    end
+  end
+
+  private
+
+  def permitted_params
+    params.require(:vehicle_import).permit(:import_file)
   end
 
 end
--- spec/controllers/vehicle_imports_controller.rb
+++ spec/controllers/vehicle_imports_controller.rb
@@ -2,10 +2,24 @@ require 'rails_helper'
 
 RSpec.describe VehicleImportsController, type: :controller do
   describe ' Import vehicles and customer data ' do
+    let(:upload_file) { Rack::Test::UploadedFile.new('spec/fixtures/vehicles_test_file.csv', 'application/csv') }
+
     describe '#new ' do 
       it 'has a 200 status code' do
         get :new
         expect(response.status).to eq(200)
+      end
+    end 
+
+    describe '#create ' do 
+      it 'redirects to vehicles page' do
+        post :create, params: {
+          vehicle_import: {
+            import_file: upload_file
+          }
+        }
+
+        expect(response.status).to eq(204)
       end
     end 
   end
